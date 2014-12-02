c23bff8324e7a304ee7ecec1fa2e3936407b09a0
maxime <maximebiron@yahoo.fr>
2014-12-02 15:33:06 +0100

    BFS

--- AStar/src/isep/projet/astar/Algo/BFS.java
+++ AStar/src/isep/projet/astar/Algo/BFS.java
@@ -3,6 +3,7 @@ package isep.projet.astar.Algo;
 import isep.projet.astar.IHM.AbstractFloor;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -14,19 +15,18 @@ import java.util.List;
 public class BFS {
 
 	private void algo(AbstractFloor StartPoint, AbstractFloor EndPoint) {
-		List<AbstractFloor> frontière = new ArrayList<>();
+		LinkedList<AbstractFloor> frontière = new LinkedList<>();
 		List<AbstractFloor> visited = new ArrayList<>();
 		frontière.add(StartPoint);
 		AbstractFloor current = StartPoint;
-		int num = 0;
+
 		while (current != EndPoint) {
-			current = frontière.get(num++);
+			current = frontière.getFirst();
 			frontière.remove(current);
 			visited.add(current);
 			for (AbstractFloor neighboor : current.getNeighboors()) {
 				if (!visited.contains(neighboor))
-					frontière.add(neighboor);
-
+					frontière.addLast(neighboor);
 			}
 		}
 	}
